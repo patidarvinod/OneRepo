8ea1271e48070f5c640b520649833d24bc217759
patidarvinod <patidarvinod4@gmail.com>
2016-11-24 22:12:26 +0530

    commit all

--- hello.c
+++ hello.c
@@ -4,7 +4,6 @@ int main()
 {
 printf("\nJust start Learning gerrit and github\n");
 printf("\nMaster\n");
-printf("\nAshu\n");
 printf("\nThis is part of MyBranch this is fix for master\n");
 return 0;
 }
